97a5a5e4b5a5b1f2b2d24ea219e0878bd957dbc3
Shaik <shaikadam642@gmail.com>
2024-05-20 00:38:50 +0530

    same again cors thing

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes")
 const postRoutes = require("./routes/postRoutes")
+const cors = require('cors');
 const port = 3000;
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use('/user', userRoutes);
 app.use('/post', postRoutes);
 
